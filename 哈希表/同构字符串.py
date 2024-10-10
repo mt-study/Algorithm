33610792e04e82dbdb3b00e58e17f8bf0a42d0f1
mt-study <656618038@qq.com>
2024-10-11 07:47:23 +0800

    24.10.10哈希表 同构字符串

---
+++ 哈希表/同构字符串.py
@@ -0,0 +1,34 @@
+"""
+题目:https://leetcode.cn/problems/isomorphic-strings/description/?envType=study-plan-v2&envId=top-interview-150
+完成时间：2024.10.10
+解题思路：定义一个字典，用来存储对应关系，如果遇到没有存储的键，用来判断值是不是已经和其他键进行了匹配，如果匹配就直接返回错误
+心得：
+
+"""
+
+
+def isIsomorphic( s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    d={}
+    for i in range(len(s)):
+        if not d.get(s[i]):
+            if t[i] in d.values():
+                return False
+            d[s[i]] = t[i]
+        else:
+            if d[s[i]] != t[i]:
+                return False
+
+    return True
+
+
+
+# print(isIsomorphic('abcd', 'abcd'))
+# print(isIsomorphic('egg', 'add'))
+# print(isIsomorphic('foo', 'bar'))
+# print(isIsomorphic('paper', 'title'))
+print(isIsomorphic('badc', 'baba'))
